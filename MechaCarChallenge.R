2cf5c344222ade03c47b0480d977f4f7624151f3
Jacobsilvester22 <silves22@msu.edu>
2022-06-29 22:12:03 -0400

    Deliverable 2 Update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,3 +11,18 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #Determine the p-value and the r-squared value for the linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_df))
+
+#Deliverable 2
+
+#Use the library() function to load the magrittr package
+library(magrittr)
+
+#Import and read in the Suspension_Coil.csv file as a table.
+SuspensionCoil_df <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#Create a total_summary dataframe using the summarize() function to get the mean, median, variance, and standard deviation of the suspension coil’s PSI column
+SuspensionCoil_summary <- SuspensionCoil_df %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+#Create a lot_summary dataframe using the group_by() and the summarize() functions to group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil’s PSI column.
+SuspensionCoil_summary_by_lot <- SuspensionCoil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
